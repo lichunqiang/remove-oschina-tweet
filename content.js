1ced0000b0facb52b2152508503cad13d6427155
light <light-li@hotmail.com>
2014-08-11 12:23:00 +0800

    init

---
+++ content.js
@@ -0,0 +1,8 @@
+var tweetArea = document.getElementById('DynaInfo');
+tweetArea && (tweetArea.remove(), done());
+
+function done() {
+	console.log('-----------------------');
+	console.log('       done!           ');
+	console.log('-----------------------');
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+remove-oschina-tweet
+---------
+
+> 去除oschina首页的动弹列表
+
+[![](screenshot.jpg)][install]
+
+这下世界可以清静了
